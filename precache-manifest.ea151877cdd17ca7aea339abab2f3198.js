d46009c852cac396acbb4fdd3e931c514f12f693
riazmasud <masud.riaz@gmail.com>
2020-02-15 15:53:26 -0500

    Updates

---
+++ precache-manifest.ea151877cdd17ca7aea339abab2f3198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c788a4669f7c406e85fe806479c82006",
+    "url": "/riazmasud.github.io/riazmasud/my-react-app/index.html"
+  },
+  {
+    "revision": "aff661ce66f3812a898e",
+    "url": "/riazmasud.github.io/riazmasud/my-react-app/static/css/main.c291014f.chunk.css"
+  },
+  {
+    "revision": "0f582b27dcb4a5e73147",
+    "url": "/riazmasud.github.io/riazmasud/my-react-app/static/js/2.a384b3aa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/riazmasud.github.io/riazmasud/my-react-app/static/js/2.a384b3aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff661ce66f3812a898e",
+    "url": "/riazmasud.github.io/riazmasud/my-react-app/static/js/main.b84ab580.chunk.js"
+  },
+  {
+    "revision": "e3088fe4d6901eb06cc1",
+    "url": "/riazmasud.github.io/riazmasud/my-react-app/static/js/runtime-main.83dc7a2b.js"
+  }
+]);
